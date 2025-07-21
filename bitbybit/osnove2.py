adc582a7c0421dfa14fc0d751f3bde090c2d7db6
TjanL <tljubesek@gmail.com>
2025-07-21 20:33:30 +0200

    osnove 2

--- bitbybit/osnove2.py
+++ bitbybit/osnove2.py
@@ -1,3 +1,23 @@
-# funkcije
-def funkcija(x):
-    return x + 1
+def izpisi_vrsto(text):
+    print(text + " ", end="")
+
+
+def narisi_kvadrat(visina, sirina, znak="#"):
+    for v in range(visina):
+        for s in range(sirina):
+            izpisi_vrsto(znak)
+        print()
+    print()
+
+
+file = open("output.txt", "a")
+file.write("Hello world!")
+file.close()
+
+file = open("output.txt", "r")
+print(file.read())
+file.close()
+
+with open("output.txt", "r") as file:
+    file.write("Hello world!")
+    print(file.read())
